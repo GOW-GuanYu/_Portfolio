9a7a0f41d83f7edce9b86a28814e770acbe87b16
Arthur Yvens <arthuryvens85@gmail.com>
2023-05-20 18:28:38 -0300

    commit
    .

--- main/script.js
+++ main/script.js
@@ -1,22 +1,24 @@
-function ativatyping(elemento){
-    const arrTexto = elemento.innerHTML.split('')
-    elemento.innerHTML = ''
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra
-        }, 75 * i)
-    })
-}
+    function ativatyping(elemento){
+        const arrTexto = elemento.innerHTML.split('')
+        elemento.innerHTML = ''
+        arrTexto.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra
+            }, 75 * i)
+        })
+    }
 
-const titulo = document.querySelector('.typing')
-ativatyping(titulo)
+    const titulo = document.querySelector('.typing')
+    ativatyping(titulo)
 
 //adicionando "animação" menu
 
 const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao')
 
 ativaMenu.addEventListener('click', ()=>(
-    ativaMenu.classList.toggle('fa-x') //adiciona fa-x no font awesome
-))
 
+    ativaMenu.classList.toggle('fa-x')(
+    navMenu.classList.toggle('ativado')
 
+)))
